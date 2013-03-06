50380ea85c77a406fd439ad6978f48f1ccf38fe2
Steve Frank <steve@steve-xps.(none)>
2013-03-06 15:24:05 -0500

    added postgres

--- web.js
+++ web.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var pg = require('pg');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+//  response.send("DB: [" + process.env.DATABASE_URL + "]");
+var client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+
+console.log(process.env.DATABASE_URL);
+
+//  pg.connect(process.env.DATABASE_URL, function(err, client) {
+    var query = client.query('SELECT * FROM users');
+    var html = '';
+
+    query.on('row', function(row) {
+      console.log(row.username);
+      html += row.username + ',';
+    });
+
+    query.on('end', function() { 
+      client.end();
+      response.send('ROWS: [' + html + ']');
+    });
+
+    //response.send('ROWS: [' + html + ']');
+//  });
+
 });
 
 var port = process.env.PORT || 5000;
